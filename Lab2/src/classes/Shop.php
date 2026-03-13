b315dce980a2cd581d9406f6cf1320187432b428
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2026-03-13 15:43:37 +0100

    feat(res-wiz): Implement Shop.php

--- Lab2/src/classes/Shop.php
+++ Lab2/src/classes/Shop.php
@@ -4,10 +4,32 @@ namespace classes;
 
 class Shop
 {
-    private int $step = 1;
-
     public function __construct()
     {
+        if(isset($_SESSION["step"])){
+            $_SESSION["step"] = 1;
+        }
+
+        if(isset($_SESSION["step"])){
+            $_SESSION["reservation"] = [];
+        }
+    }
+
+    public function getStep() : int
+    {
+        return $_SESSION["step"];
+    }
+
+    public function saveStep1(array $data) : void
+    {
+        $_SESSION['reservation']['phase'] = $data['phase'] ?? '';
+        $_SESSION['reservation']['email'] = $data['email'] ?? '';
+        $_SESSION['step'] = 2;
+    }
+
+    public function getReservationData() : array
+    {
+        return $_SESSION['reservation'];
     }
 
 }
